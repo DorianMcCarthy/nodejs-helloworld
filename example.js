287fea08a5b102ab6fa486df311ead8f1c6142d8
DorianMcCarthy <chronicbase@gmail.com>
2015-07-04 04:34:57 +1000

    WORK IN PROGRESS

---
+++ example.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '46.101.56.136');
+console.log('Server running at 46.101.56.136');
